be8d426c5ea03cca5bfbdccbf31ff156b5120487
Matt Holmes <holmesm8@gmail.com>
2020-02-03 14:23:35 -0700

    Add model testing and the model for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Review, type: :model do
   describe "validations" do
     # it {should validate_presence_of :image}
     it {should validate_presence_of :title}
-    it {should validate_presence_of :raiting}
+    it {should validate_presence_of :rating}
     it {should validate_presence_of :content}
   end
 
