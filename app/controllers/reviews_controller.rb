dbc7c011514324e5b6dbaa85a002214d0bfdbdc0
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-05 14:44:22 -0700

    Add catch for wrong review input

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,12 @@ class ReviewsController < ApplicationController
 
   def create
     review = Review.new(review_params)
-    review.save
-    redirect_to "/shelters/#{review.shelter_id}"
+    if review.save
+	redirect_to "/shelters/#{review.shelter_id}"
+    else
+	flash[:notice] = "Fields required: Title, Rating, Content"
+	redirect_to "/shelters/#{review.shelter_id}/reviews/new"
+    end
   end
 
   def edit
