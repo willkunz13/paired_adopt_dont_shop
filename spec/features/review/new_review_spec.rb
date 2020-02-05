257f747cb137713ac32b3870b7000f31bbde76aa
Matt Holmes <holmesm8@gmail.com>
2020-02-04 20:04:59 -0700

    Add testing for user story 5

--- spec/features/review/new_review_spec.rb
+++ spec/features/review/new_review_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-  
+
 RSpec.describe "review creation page", type: :feature do
   context "as a visitor" do
     it "can create a new review" do
@@ -10,22 +10,56 @@ RSpec.describe "review creation page", type: :feature do
                                 zip: "80202")
 
       visit "/shelters/#{shelter1.id}"
-	click_link "Add Review"
+	    click_link "Add Review"
 
-	expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/new")
+	    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/new")
 
-	fill_in 'Title', with: 'review_test'
+	    fill_in 'Title', with: 'review_test'
       fill_in 'Rating', with: '9'
       fill_in 'content', with: 'test content'
-	fill_in "image", with: 'https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666'
-	click_button "Submit"
-	expect(current_path).to eq("/shelters/#{shelter1.id}")
-	expect(page).to have_content("review_test")
-      expect(page).to have_content("9")
-      expect(page).to have_content("test content")	
-	expect(page).to have_css("img[src*='https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666']")
+	    fill_in "image", with: 'https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666'
 
+	    click_button "Submit"
+
+	    expect(current_path).to eq("/shelters/#{shelter1.id}")
+	    expect(page).to have_content("review_test")
+      expect(page).to have_content("9")
+      expect(page).to have_content("test content")
+	    expect(page).to have_css("img[src*='https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666']")
     end
   end
 end
 
+RSpec.describe "review edit page", type: :feature do
+  context "as a visitor" do
+    it "can edit a review" do
+      shelter1 = Shelter.create(name: "Mike's Shelter",
+                                address: "1331 17th Street",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+
+      review1 = Review.create(title: "Love this shelter!",
+                              rating: 5,
+                              content: "I feel that this shelter has given me so many pets it should be illegal.",
+                              image: "https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666",
+                              shelter_id: shelter1.id)
+
+      visit "/shelters/#{shelter1.id}"
+	    click_link "Edit Review"
+
+	    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/edit")
+
+	    fill_in 'Title', with: 'review_test'
+      fill_in 'Rating', with: '9'
+      fill_in 'content', with: 'test content'
+
+	    click_button "Submit"
+
+	    expect(current_path).to eq("/shelters/#{shelter1.id}")
+	    expect(page).to have_content("review_test")
+      expect(page).to have_content("9")
+      expect(page).to have_content("test content")
+    end
+  end
+end
