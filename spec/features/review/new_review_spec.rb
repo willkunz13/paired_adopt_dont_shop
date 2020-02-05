a09a01346a9c611b334b5d9350fc3e1f8234b609
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-05 12:50:48 -0700

    Add test for failed review edge case

--- spec/features/review/new_review_spec.rb
+++ spec/features/review/new_review_spec.rb
@@ -27,7 +27,28 @@ RSpec.describe "review creation page", type: :feature do
       expect(page).to have_content("test content")
 	    expect(page).to have_css("img[src*='https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666']")
     end
-  end
+it "can catch incorrect reviews" do
+      shelter1 = Shelter.create(name: "Mike's Shelter",
+                                address: "1331 17th Street",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+
+      visit "/shelters/#{shelter1.id}"
+            click_link "Add Review"
+
+            expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/new")
+
+            fill_in 'Title', with: 'review_test'
+     	    fill_in 'Rating', with: '9'
+            fill_in "image", with: 'https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_77846398eureka2_x850.jpg?v=1554665666'
+
+            click_button "Submit"
+
+            expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/new")
+            expect(page).to have_content("Fields required: Title, Rating, Content")
+    end
+ end
 end
 
 RSpec.describe "review edit page", type: :feature do
