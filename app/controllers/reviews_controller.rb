88263a41b8f6572d01d75e1256e9472471a0d49a
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-05 15:11:17 -0700

    Reorganize removal of moved files

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,8 +21,12 @@ class ReviewsController < ApplicationController
     review = Review.find(params[:id])
     review.update(review_params)
 
-    review.save
-    redirect_to "/shelters/#{review.shelter_id}"
+    if review.save
+	    redirect_to "/shelters/#{review.shelter_id}"
+    else
+	flash[:notice] = "Fields required: Title, Rating, Content"
+        redirect_to "/shelters/#{review.shelter_id}/reviews/#{review.id}/edit"
+    end
   end
 
   def destroy
