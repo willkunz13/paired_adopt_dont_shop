393c1f1d6b0499ef1c342490c5c6c100fb3db069
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-06 15:35:04 -0700

    Add routes to favorites index page

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
   delete '/shelters/reviews/:id', to: 'reviews#destroy'
 
 	patch '/favorites/:pet_id', to: 'favorites#update'
+	get '/favorites', to: 'favorites#index'
 end
