c4ac14475390272b6f650394982c710ddb15a0c5
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-06 13:51:02 -0700

    Add favorites model Poro

---
+++ app/models/favorites.rb
@@ -0,0 +1,11 @@
+class Favorites 
+  attr_reader :contents
+
+  def initialize(initial_contents)
+    @contents = initial_contents || Array.new 
+  end
+
+  def total_count
+    contents.size
+  end
+end
