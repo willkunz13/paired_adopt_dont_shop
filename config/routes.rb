21babb37658e102eb056c8ace751ae26b5b64861
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-07 15:39:50 -0700

    Add delete method to routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
 
 	patch '/favorites/:pet_id', to: 'favorites#update'
 	get '/favorites', to: 'favorites#index'
+	delete '/favorites/:id', to: 'favorites#destroy'
 end
