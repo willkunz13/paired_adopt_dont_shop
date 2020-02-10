e6611e2068a7caf71997ff370f20ab3b45ea24dd
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-10 12:46:05 -0700

    Add fix for relationships

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :age, :sex, :description, :adoptable
   belongs_to :shelter
+	has_many :application_pets
+	has_many :applications, through: :application_pet
 end
